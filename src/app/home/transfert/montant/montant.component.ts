42e0ae750f772c830882ad4d6a629265f7bbd46b
mariemesembene <mariemesembenediop@gmail.com>
2022-07-01 15:00:11 +0000

    dd

--- src/app/home/transfert/montant/montant.component.ts
+++ src/app/home/transfert/montant/montant.component.ts
@@ -1,14 +1,70 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/internal/operators';
+import { ApiService } from 'src/app/api.service';
+import { AppUtils } from 'src/app/app.utils';
 
 @Component({
-  selector: 'app-montant',
+  selector: 'app-transfert-montant',
   templateUrl: './montant.component.html',
   styleUrls: ['./montant.component.scss'],
 })
 export class MontantComponent implements OnInit {
 
-  constructor() { }
+  user: any;
+  comptes: any[] = [];
+  principalCompte: any;
+  principalOperateur: any;
+  solde = 0;
 
-  ngOnInit() {}
+  contact: any;
+  moyen: any;
+
+  montantEnvoyeControl: FormControl;
+  montantRecuControl: FormControl;
+  montantEnvoye = 0;
+  montantRecu = 0;
+  frais = 0;
+  fraisPercent: any;
+
+  //
+  dispatching: any[] = [];
+  comptesToutConsomme: any[];
+  montantDispatche = 0;
+
+  isValid =false;
+  erreur: any;
+  interoperable = false;
+
+  transfert: any;
+  showSuccess = false;
+
+  // Private
+  private unsubscribeAll: Subject<any>;
+
+  constructor(
+    private router: Router,
+    private apiService: ApiService,
+    private loadingController: LoadingController,
+    private alertController: AlertController
+    ) {
+      this.unsubscribeAll = new Subject();
+    }
+
+ngOnInit(): void {
+
+}
+
+
+
+  goToHomePage(): void{
+    this.showSuccess=false;
+    setTimeout(() => {
+      this.router.navigateByUrl('/home');
+    }, 300);
+  }
 
 }
--- src/app/home/transfert/transfert.page.ts
+++ src/app/home/transfert/transfert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-transfert',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransfertPage implements OnInit {
 
-  constructor() { }
+  public page: string;
+
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.page = this.activatedRoute.snapshot.paramMap.get('page');
   }
 
 }
