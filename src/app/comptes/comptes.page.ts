fa2752fc8b165fe44f848ea52f2d3f52edd833a2
Saly <saly@applize.tech>
2022-07-01 13:23:49 +0000

    Login, start comptes

--- src/app/comptes/comptes.page.ts
+++ src/app/comptes/comptes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-comptes',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComptesPage implements OnInit {
 
-  constructor() { }
+  modalOpened = false;
+  selectedMoyen: any;
+
+  compteForm: FormGroup;
+  showSuccess = false;
+
+  comptes: any[] = [];
+
+  mobiles = [{"nom":"OM", "number": 1, "logo": "assets/images/Orange-Money.png"}]
+  constructor(
+    private _formBuilder: FormBuilder
+
+  ) { }
 
   ngOnInit() {
+  }
+
+
+  addMoyen(moyen) {
+    this.modalOpened = true;
+    this.selectedMoyen = moyen;
+    //if(this.selectedMoyen.type === 'mobile'){
+      this.compteForm = this._formBuilder.group({
+        numero: ['', Validators.required],
+        code: ['', Validators.required],
+        moyen: [moyen.id, Validators.required],
+      });
+      console.log('compteForm', this.compteForm);
+   // }
+  }
+
+  isCompteAdded(){
+
   }
 
 }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -14,6 +14,7 @@ export class LoginPage implements OnInit {
     ) { }
 
   ngOnInit() {
+    this.initForm();
   }
 
 
